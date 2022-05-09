e1dcd30109a05d9dbada04c99cb9d48914fb6298
Volodymyr Shevchuk <dosandkv@gmail.com>
2022-05-09 20:43:38 +0300

    https://github.com/dosandk/online-editor-tasks/issues/7

--- basic/04-revertCase/index.spec.js
+++ basic/04-revertCase/index.spec.js
@@ -1,6 +1,10 @@
-import { revertCase } from './solution/index.js';
+import { revertCase } from './index.js';
 
 describe('revertCase', () => {
+  it('should correctly handle whitespace characters', () => {
+    expect(revertCase(' ')).toBe(' ');
+  });
+
   it('should return a string with letters in opposite register', () => {
     expect(revertCase('Hello World')).toBe('hELLO wORLD');
     expect(revertCase('HeLlO WoRlD')).toBe('hElLo wOrLd');
