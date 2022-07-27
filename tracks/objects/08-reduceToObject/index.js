768de52b219561508213889368b9595b38633267
Maksym Tutskyi <maksym.tutskyi@eleks.com>
2022-07-27 16:24:38 +0300

    Fixed readme and comments typos

--- tracks/objects/08-reduceToObject/index.js
+++ tracks/objects/08-reduceToObject/index.js
@@ -1,9 +1,3 @@
-export const reduceToObject = (orders = []) => {
-  const products = {
-    pId1: { name: "Big Mac", price: 90 },
-    pId2: { name: "Big Tasty", price: 120 },
-    pId3: { name: "Mc Chicken", price: 60 },
-    pId4: { name: "Fille O Fish", price: 45 },
-  };
+export const reduceToObject = (orders = [], products = {}) => {
 
 };
