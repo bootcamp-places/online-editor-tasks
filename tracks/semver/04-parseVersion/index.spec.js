9c485a6b31272327f0a7d7941d329b8db3565927
A. Merezhanyi <anatoliy.merezhanyi@eleks.com>
2022-11-07 13:40:07 +0100

    started semver#4

---
+++ tracks/semver/04-parseVersion/index.spec.js
@@ -0,0 +1,9 @@
+import { parseVersion } from './solution/index.js';
+
+describe('semver/parseVersion', () => {
+  it('should be defined', () => {
+    expect(parseVersion).toBeDefined();
+  });
+
+  // [your tests here]
+});
---
+++ tracks/semver/04-parseVersion/solution/index.js
@@ -0,0 +1,3 @@
+export const parseVersion = () => {
+  // [your solution here]
+};
