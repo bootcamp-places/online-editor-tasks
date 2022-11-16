b7bd8c1a260b4125f8cb31d86b56cf3eaf81a5c2
A. Merezhanyi <anatoliy.merezhanyi@eleks.com>
2022-11-16 10:38:11 +0100

    Init task

---
+++ tracks/semver/05-intervalSelect/index.spec.js
@@ -0,0 +1,9 @@
+import { intervalSelect } from "./solution/index.js";
+
+describe("semver/intervalSelect", () => {
+  it("should be defined", () => {
+    expect(intervalSelect).toBeDefined();
+  });
+
+  // [your tests here]
+});
---
+++ tracks/semver/05-intervalSelect/solution/index.js
@@ -0,0 +1,3 @@
+export const intervalSelect = () => {
+  // [your solution here]
+};
