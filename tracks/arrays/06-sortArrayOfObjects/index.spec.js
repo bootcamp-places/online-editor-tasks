05b76dec19ae03a5575b2ced5d2b062f59a4c552
A. Merezhanyi <anatoliy.merezhanyi@eleks.com>
2022-12-06 12:34:07 +0100

    started a task

---
+++ tracks/arrays/06-sortArrayOfObjects/index.spec.js
@@ -0,0 +1,9 @@
+import { sortArrayOfObjects } from './solution/index.js';
+
+describe('arrays/sortArrayOfObjects', () => {
+  it('should be defined', () => {
+    expect(sortArrayOfObjects).toBeDefined();
+  });
+
+  // [your tests here]
+});
---
+++ tracks/arrays/06-sortArrayOfObjects/solution/index.js
@@ -0,0 +1,3 @@
+export const sortArrayOfObjects = () => {
+  // [your solution here]
+};
