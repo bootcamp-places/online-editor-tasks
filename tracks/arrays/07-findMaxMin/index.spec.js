40e2fc7e1705a09e1b4c6e3a63ab432b116ad7d0
A. Merezhanyi <anatoliy.merezhanyi@eleks.com>
2022-12-09 10:16:36 +0100

    started new task

---
+++ tracks/arrays/07-findMaxMin/index.spec.js
@@ -0,0 +1,9 @@
+import { findMaxMin } from './solution/index.js';
+
+describe('arrays/findMaxMin', () => {
+  it('should be defined', () => {
+    expect(findMaxMin).toBeDefined();
+  });
+
+  // [your tests here]
+});
---
+++ tracks/arrays/07-findMaxMin/solution/index.js
@@ -0,0 +1,3 @@
+export const findMaxMin = () => {
+  // [your solution here]
+};
