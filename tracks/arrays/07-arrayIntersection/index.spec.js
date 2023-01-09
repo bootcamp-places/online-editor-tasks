1594b37e35a59f3956b749b658f05cc71aaab6a5
A. Merezhanyi <anatoliy.merezhanyi@eleks.com>
2023-01-09 13:29:12 +0100

    started task

---
+++ tracks/arrays/07-arrayIntersection/index.spec.js
@@ -0,0 +1,9 @@
+import { arrayIntersection } from './solution/index.js';
+
+describe('arrays/arrayIntersection', () => {
+  it('should be defined', () => {
+    expect(arrayIntersection).toBeDefined();
+  });
+
+  // [your tests here]
+});
---
+++ tracks/arrays/07-arrayIntersection/solution/index.js
@@ -0,0 +1,3 @@
+export const arrayIntersection = () => {
+  // [your solution here]
+};
