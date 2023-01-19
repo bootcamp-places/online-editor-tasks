f8a217ae3ff0b1b05f7d4b4341e2354af0515e19
A. Merezhanyi <anatoliy.merezhanyi@eleks.com>
2023-01-19 16:37:19 +0100

    finalised

--- tracks/basic/17-pangram/solution/index.js
+++ tracks/basic/17-pangram/solution/index.js
@@ -1,19 +1,5 @@
-// export const pangram = (text = "") => {
-//   const stripText = text.toLowerCase().match(/[а-ґ]/gi, "");
-//   const uniqueCharsSize = new Set(stripText).size;
-//   return 33 === uniqueCharsSize;
-// };
 export const pangram = (text = "") => {
-  if (text.trim() === "") {
-    return false;
-  }
-  const uniqueChars = text
-    .toLowerCase()
-    .split("")
-    .reduce(
-      (accumulator, currentLetter) => (accumulator[currentLetter] = ""),
-      {}
-    );
-  const uniqueCharsSize = Object.keys(uniqueChars).length;
+  const stripText = text.toLowerCase().match(/[а-ґ]/gi, "");
+  const uniqueCharsSize = new Set(stripText).size;
   return 33 === uniqueCharsSize;
 };
