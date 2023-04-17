37ae4c3c9a6f3926152f285bd662ab1b9bf4fc2a
A. Merezhanyi <anatoliy.merezhanyi@eleks.com>
2023-04-17 13:40:00 +0200

    initial commit

---
+++ tracks/regexp/07-logScan/index.spec.js
@@ -0,0 +1,9 @@
+import { logScan } from './solution/index.js';
+
+describe('regexp/logScan', () => {
+  it('should be defined', () => {
+    expect(logScan).toBeDefined();
+  });
+
+  // [your tests here]
+});
---
+++ tracks/regexp/07-logScan/solution/index.js
@@ -0,0 +1,3 @@
+export const logScan = () => {
+  // [your solution here]
+};
